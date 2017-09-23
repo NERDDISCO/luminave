578e4b7954afeb68d391a0f20bbcf8bd9823ab7f
Gregor Adams <greg@pixelass.com>
2017-09-23 17:38:16 +0200

    feat(components): added connect button
    A button that shows connection status

---
+++ src/components/connect-button/index.js
@@ -0,0 +1,25 @@
+import { Element as PolymerElement } from '/node_modules/@polymer/polymer/polymer-element.js'
+
+export class ConnectButton extends PolymerElement {
+
+  constructor() {
+    super();
+  }
+  ready(){
+    super.ready()
+  }
+
+  handleClick(e) {
+    if (this.connected){
+      this.dispatchEvent(new CustomEvent('disconnect', { detail: { connected: false } }))
+    } else {
+       this.dispatchEvent(new CustomEvent('connect', { detail: { connected: true } }))
+    }
+  }
+
+  static get template() {
+    return `
+      <button on-click="handleClick">Connected: [[connected]]</button>
+    `
+  }
+}
