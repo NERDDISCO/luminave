4c18b09186a1e5b07ad1dde9ccddc6ef2b0a3a42
Tim Pietrusky <timpietrusky@gmail.com>
2017-10-20 17:27:37 +0200

    Moved server into it's own folder.

---
+++ server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/kabukky/httpscerts"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Check if the cert files are available.
+	err := httpscerts.Check("cert.pem", "key.pem")
+	// If they are not available, generate new ones.
+	if err != nil {
+		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:1337")
+		if err != nil {
+			log.Fatal("Error: Couldn't create https certs.")
+		}
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	// HTTP
+	//log.Fatal(http.ListenAndServe(":8081", nil))
+
+	// HTTPS
+	log.Fatal(http.ListenAndServeTLS(":1337", "cert.pem", "key.pem", nil))
+}
