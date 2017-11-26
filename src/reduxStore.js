b647feb14b58182b2524f021d6c8249ad381ce65
Tim Pietrusky <timpietrusky@gmail.com>
2017-11-26 15:38:27 +0100

    refactor(redux-mixin): Remove unused actions
    Removed actions from the ReduxMixin because they are loaded per component anyway

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,28 +1,15 @@
-import { dedupingMixin } from '/node_modules/@polymer/polymer/lib/utils/mixin.js'
-
 import { createStore, combineReducers } from '/libs/redux/index.js'
-
 import * as reducers from './reducers/index.js'
-import * as actions from './actions/index.js'
 
 export const store = createStore(
   combineReducers({
     ...reducers
-  }, {
-    connectManager: {usb: false, bluetooth: true}
   })
-
 )
 
+// @see https://github.com/tur-nr/polymer-redux#polymerredux
 const reduxMixin = PolymerRedux(store)
 
-/* @mixinFunction */
-const actionsMixin = (superClass) => {
-  return class extends reduxMixin(superClass) {
-    static get actions() {
-      return actions
-    }
-  }
-}
-
-export default dedupingMixin(actionsMixin)
+// ReduxMixin
+// @see https://github.com/tur-nr/polymer-redux#redux-mixin
+export default reduxMixin
