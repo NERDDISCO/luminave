da646a9d8f8bd12019107f22ca5930549fce43f9
Tim Pietrusky <timpietrusky@gmail.com>
2017-11-26 15:34:55 +0100

    refactor(data-binding): One-way data binding
    Only use one-was data binding

--- src/components/channel-grid/index.js
+++ src/components/channel-grid/index.js
@@ -1,5 +1,4 @@
 import { Element as PolymerElement } from '/node_modules/@polymer/polymer/polymer-element.js'
-import { html } from '/node_modules/lit-html/lit-html.js'
 import { DomRepeat } from '/node_modules/@polymer/polymer/lib/elements/dom-repeat.js'
 
 class ChannelGrid extends PolymerElement {
@@ -14,9 +13,10 @@ class ChannelGrid extends PolymerElement {
           flex: 0 0 2em;
         }
       </style>
+
       <div class="items">
-        <template is="dom-repeat" items="{{ array }}" as="channel">
-          <div class="item">{{channel}}</div>
+        <template is="dom-repeat" items="[[array]]" as="channel">
+          <div class="item">[[channel]]</div>
         </template>
       </div>
     `
--- src/components/my-view/index.js
+++ src/components/my-view/index.js
@@ -2,7 +2,6 @@ import { uuid } from '../../../libs/abcq/uuid.js'
 import { Element as PolymerElement } from '/node_modules/@polymer/polymer/polymer-element.js'
 import ReduxMixin from '../../reduxStore.js'
 import { setChannel, addUniverse, removeUniverse } from '../../actions/index.js'
-import { DEMO_UNIVERSE } from '../../constants/index.js'
 import '../channel-grid/index.js'
 import '../bpm-meter/index.js'
 import '../tap-button/index.js'
@@ -29,7 +28,7 @@ class MyView extends ReduxMixin(PolymerElement) {
   }
 
   removeUniverse(e) {
-    const {dataset} = e.target
+    const { dataset } = e.target
     this.dispatch(removeUniverse(parseInt(dataset.index, 10)))
   }
 
@@ -37,22 +36,24 @@ class MyView extends ReduxMixin(PolymerElement) {
     return `
       <button on-click="addUniverse">Add universe</button>
 
-      <template is="dom-repeat" items="{{universes}}" as="universe">
+      <template is="dom-repeat" items="[[universes]]" as="universe">
         <div>
-          {{universe.name}}
-          <button on-click="removeUniverse" data-index$="{{index}}">Remove</button>
+          [[universe.name]]
+          <button on-click="removeUniverse" data-index$="[[index]]">Remove</button>
 
           <div>
-            <channel-grid array="{{universe.channels}}"></channel-grid>
+            <channel-grid array="[[universe.channels]]"></channel-grid>
           </div>
 
         </div>
       </template>
 
       <usb-controller></usb-controller>
+
       <connect-button type="usb" label="USB"></connect-button>
       <connect-button type="bluetooth" label="BLUETOOTH"></connect-button>
-      <bpm-meter bpm="{{bpm}}"></bpm-meter>
+
+      <bpm-meter bpm="[[bpm]]"></bpm-meter>
       <tap-button></tap-button>
     `
   }
