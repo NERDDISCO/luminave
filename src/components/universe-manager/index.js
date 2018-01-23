b1927df343676039213fe6b14a41a616ef9e2b0d
Tim Pietrusky <timpietrusky@gmail.com>
2018-01-23 21:19:46 +0100

    feat(universe): Reset universe
    Added resetUniverse to reset the values in the universe

--- src/components/universe-manager/index.js
+++ src/components/universe-manager/index.js
@@ -1,7 +1,8 @@
 import { Element as PolymerElement } from '/node_modules/@polymer/polymer/polymer-element.js'
 import ReduxMixin from '../../reduxStore.js'
 import { uuidV1 } from '../../../libs/abcq/uuid.js'
-import { setChannel, addUniverse, removeUniverse } from '../../actions/index.js'
+import { setChannels, addUniverse, removeUniverse, resetAllFixtures } from '../../actions/index.js'
+import { batch, clearBatch } from '../../utils/index.js'
 
 /*
  *
@@ -26,6 +27,16 @@ class UniverseManager extends ReduxMixin(PolymerElement) {
     this.dispatch(removeUniverse(parseInt(dataset.index, 10)))
   }
 
+  resetUniverse() {
+    this.dispatch(resetAllFixtures())
+
+    // Update the channels of universe 0 with the batch of values collected for the fixtures
+    this.dispatch(setChannels(0, [...batch]))
+
+    // Reset the batch so that if a scene is done the values for the attachted fixtures are also reset
+    clearBatch()
+  }
+
   static get template() {
     return `
       <h2>Universes</h2>
@@ -35,6 +46,7 @@ class UniverseManager extends ReduxMixin(PolymerElement) {
       <template is="dom-repeat" items="{{universes}}" as="universe">
         <div>
           <h3>[[universe.name]]</h3>
+          <button on-click="resetUniverse" data-index$="[[index]]">Reset</button>
           <button on-click="removeUniverse" data-index$="[[index]]">Remove</button>
 
           <div>
