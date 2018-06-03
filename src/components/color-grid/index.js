03bc93754f6e42e8d67ca31c80a7405e1a57a115
Tim Pietrusky <timpietrusky@gmail.com>
2018-06-03 18:16:32 +0200

    feat(color-grid): Added row & column labels
    Added labels for the rows and the columns to determine which color from modV has which index. This
    can be useful for mapping lights to actual fixtures.

--- src/components/color-grid/index.js
+++ src/components/color-grid/index.js
@@ -3,12 +3,14 @@ import { DomRepeat } from '/node_modules/@polymer/polymer/lib/elements/dom-repea
 
 /*
  * Show colors in a grid
+ *
+ * @TODO: Move the label (borth column and row) into their own modules and make them configurable
+ * @TODO: Fix the performance
  */
 class ColorGrid extends PolymerElement {
   static get properties() {
     return {
-      width: Number,
-      height: Number,
+      rows: Number,
       colors: Array
     }
   }
@@ -17,8 +19,8 @@ class ColorGrid extends PolymerElement {
    * Change CSS properties based on properties of the component that can be
    * changed during runtime
    */
-  computeGridVars(width) {
-    const vars = { '--width': width }
+  computeGridVars(rows) {
+    const vars = { '--rows': rows }
 
     return Object.keys(vars).map(key => [key, vars[key]].join(':')).join(';')
   }
@@ -39,26 +41,31 @@ class ColorGrid extends PolymerElement {
 
   /*
    * Convert simple array of colors into Array of Objects which can be used by dom-repeat
+   *
+   * @TODO See if this is called over and over again when the colors are changing
    */
   _toColorArray(colors) {
     const colorChunks = []
     let i = 0
 
     while (i < colors.length) {
-      const chunk = {
-        color: colors.slice(i, i + 3)
-      }
-
+      const chunk = { color: colors.slice(i, i + 3) }
       colorChunks.push(chunk)
-
       i += 3
     }
 
     return colorChunks
   }
 
+  /*
+   * Set the label of a color
+   *
+   * @TODO: See if this is called over and over again when the colors are changing
+   */
   computeLabel(index) {
-    return index + 1
+    const realIndex = index + 1
+
+    return `${realIndex}`
   }
 
   static get template() {
@@ -66,21 +73,50 @@ class ColorGrid extends PolymerElement {
       <style>
         .container {
           display: grid;
-          grid-template-rows: repeat(var(--width), auto);
+          grid-template-rows: repeat(var(--rows), auto);
           grid-auto-flow: column;
           column-gap: 0;
-          row-gap: var(--padding-basic);
+          row-gap: calc(var(--padding-basic) / 2);
+
+          counter-reset: header;
+        }
+
+        .container.left {
+          float: left;
+        }
+
+        .container.right {
+          float: right;
+        }
+
+        .row-label {
+          height: 25px;
+          width: 35px;
+          font-size: .9em;
         }
 
         .item {
+          position: relative;
           background: rgb( var(--red), var(--green), var(--blue));
           height: 25px;
-          width: 55px;
+          width: 40px;
+        }
+
+        .item:nth-child(4n-3):before {
+          counter-increment: header;
+          content: counter(header);
+          position: absolute;
+          top: calc(var(--padding-basic) * -6);
+          overflow: visible;
+          background: var(--background-dark);
+          color: var(--color-light);
+          padding: var(--padding-basic);
+          font-size: .7em;
         }
 
         .label {
-          font-size: .8em;
-          padding: 0 0 0 calc(var(--padding-basic));
+          font-size: .7em;
+          padding: 0 0 0 calc(var(--padding-basic) / 2);
           background: var(--background-dark);
           color: var(--color-light);
           height: 100%;
@@ -90,7 +126,25 @@ class ColorGrid extends PolymerElement {
         }
       </style>
 
-      <div class="container" style="{{computeGridVars(width)}}">
+      <div class="container left" style="{{computeGridVars(rows)}}">
+          <div class="row-label">
+            <span class="label">a</span>
+          </div>
+
+          <div class="row-label">
+            <span class="label">b</span>
+          </div>
+
+          <div class="row-label">
+            <span class="label">c</span>
+          </div>
+
+          <div class="row-label">
+            <span class="label">d</span>
+          </div>
+      </div>
+
+      <div class="container" style="{{computeGridVars(rows)}}">
 
         <template is="dom-repeat" items={{_toColorArray(colors)}} as="element">
           <div class="item" style="{{computeItemVars(element.color)}}">
