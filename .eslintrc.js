44d0dae341f5b3f5dab936747ad7e3c7173cb11e
Tim Pietrusky <timpietrusky@gmail.com>
2018-09-05 08:00:27 +0200

    build(eslint): Deactivated no-underscore-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -155,7 +155,7 @@ module.exports = {
         'no-trailing-spaces': 'off',
         'no-undef-init': 'error',
         'no-undefined': 0,
-        'no-underscore-dangle': 'error',
+        'no-underscore-dangle': 'off',
         'no-unmodified-loop-condition': 'error',
         'no-unneeded-ternary': 'error',
         'no-unused-expressions': 'error',
