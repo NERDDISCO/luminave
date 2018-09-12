a68de327d5d9540b88c996f32f0d3480eac835e3
Tim Pietrusky <timpietrusky@gmail.com>
2018-09-12 23:53:50 +0200

    refactor(app): Space between components
    Added some spave between the menu and the dashboard

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-import { html, PolymerElement } from '/node_modules/@polymer/polymer/polymer-element.js'
+import { LitElement, html } from '/node_modules/@polymer/lit-element/lit-element.js'
 
 import './components/luminave-menu/index.js'
 import './components/luminave-dashboard/index.js'
 
-class Luminave extends PolymerElement {
-  constructor() {
-    super()
-  }
-
-  static get template() {
+class Luminave extends LitElement {
+  _render() {
     return html`
       <luminave-menu></luminave-menu>
+
+      <ui-spacer></ui-spacer>
+      <ui-spacer></ui-spacer>
+      <ui-spacer></ui-spacer>
+      <ui-spacer></ui-spacer>
+      <ui-spacer></ui-spacer>
+
       <luminave-dashboard></luminave-dashboard>
     `
   }
