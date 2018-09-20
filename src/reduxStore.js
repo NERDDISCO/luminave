c26a0a741bad67387de908b6888c078ab9e23fbb
Tim Pietrusky <timpietrusky@gmail.com>
2018-09-20 13:49:22 +0200

    refactor(*): Removed polymer-redux
    Instead of polymer-redux we are using the pwa-helper "connect" to talk with the Redux store.
    
    Fixes #43

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,4 +1,3 @@
-import PolymerRedux from '/node_modules/polymer-redux/polymer-redux.js'
 import { createStore, combineReducers, applyMiddleware } from '/libs/redux/index.js'
 
 import * as reducers from './reducers/index.js'
@@ -15,10 +14,3 @@ export const store = createStore(
   localStorage.getItem(STORAGE_STATE) ? JSON.parse(localStorage.getItem(STORAGE_STATE)) : {},
   applyMiddleware(thunk)
 )
-
-// @see https://github.com/tur-nr/polymer-redux#polymerredux
-export const reduxMixin = PolymerRedux(store)
-
-// ReduxMixin
-// @see https://github.com/tur-nr/polymer-redux#redux-mixin
-export default reduxMixin
