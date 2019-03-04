c0625d920fa1f4ca2baa829bc114eee6852b4e3b
Tim Pietrusky <timpietrusky@gmail.com>
2019-03-04 15:23:46 +0100

    fix(directive): React to "undefined" String
    Instead of just creating a default value for typeof undefined, we also check now if the variable has
    the String value "undefined" as it's value.

--- src/directives/default-value.js
+++ src/directives/default-value.js
@@ -4,7 +4,7 @@ import { directive } from 'lit-html/lit-html.js'
  * Set a default value if the provided value is undefined
  */
 export const defaultValue = (value, defaultVal) => directive(part => {
-    if (value === undefined) {
+    if (value === undefined || value === 'undefined') {
         value = defaultVal
     }
 
