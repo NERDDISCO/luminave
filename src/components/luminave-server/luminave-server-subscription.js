7c4218c3c6f0020e3af29edec45d83cc09614238
Tim Pietrusky <timpietrusky@gmail.com>
2019-04-27 21:39:44 +0200

    feat(thorium): Dynamic animation
    Update the keyframes & duration of an animation by using values from Thorium.
    
    #100 #99

--- src/components/luminave-server/luminave-server-subscription.js
+++ src/components/luminave-server/luminave-server-subscription.js
@@ -2,14 +2,17 @@
 import { LitElement, html, css } from 'lit-element/lit-element.js'
 import gql from 'graphql-tag'
 import { ApolloSubscription } from '@apollo-elements/lit-apollo'
+import { setAnimation } from '../../actions'
+
+
+// TODO: Remove later
+import { connect } from 'pwa-helpers/connect-mixin.js'
+import { store } from '../../reduxStore.js'
+import Color from 'color'
 
 // Compute graphql documents statically for performance
 const subscription = gql`
   subscription {
-    # timelineScenesUpdated {
-    #   name
-    # }
-
     rawUpdated {
       data
     }
@@ -19,7 +22,15 @@ const subscription = gql`
 /**
  * Subsribe to the luminave-server to get updates
  */
-class LuminaveServerSubscription extends ApolloSubscription {
+class LuminaveServerSubscription extends connect(store)(ApolloSubscription) {
+
+  constructor() {
+    super()
+
+    this.lastValues = {
+      dimmer: 0
+    }
+  }
   
   static get properties() {
     return {
@@ -38,8 +49,8 @@ class LuminaveServerSubscription extends ApolloSubscription {
     }
 
     if (changedProps.has('data')) {
-      console.log(this.data)
-      // this.scenes = this.data.timelineScenesUpdated.map(scene => scene.name)
+      console.log(this.data.rawUpdated.data)
+      this._setAnimation(this.data.rawUpdated.data)
     }
 
     return (
@@ -54,6 +65,190 @@ class LuminaveServerSubscription extends ApolloSubscription {
     if (changedProps.has('client')) {
       this.subscription = subscription
     }
+  }
+
+  _stateChanged() {
+
+  }
+
+  _setAnimation(data) {
+
+    const { intensity, transitionDuration, color, action, actionStrength } = data
+
+    const dimmer = Math.round(255 * intensity)
+
+    // TODO: duration should be in ms, not in seconds (luminave only supports seconds as of now)
+    const duration = (transitionDuration / 1000).toFixed(1)
+
+    const _color = Color(color).rgb().array()
+
+    let keyframes = {
+      0: { 
+        dimmer: dimmer,
+        color: _color
+      },
+      1: { 
+        dimmer: dimmer,
+        color: _color
+      }
+    }
+
+    switch (action) {
+      case 'normal':
+        keyframes = {
+          0: {
+            dimmer: dimmer,
+            color: _color
+          },
+          1: {
+            dimmer: dimmer,
+            color: _color
+          }
+        }
+        break
+       
+     case 'fade':
+        // fade down
+        if (dimmer === 0) {
+          keyframes = {
+            0: {
+              dimmer: this.lastValues.dimmer,
+              color: _color
+            },
+            1: {
+              dimmer: dimmer,
+              color: _color
+            }
+          }
+        // fade up
+        } else {
+          keyframes = {
+            0: {
+              dimmer: this.lastValues.dimmer,
+              color: _color
+            },
+            1: {
+              dimmer: dimmer,
+              color: _color
+            }
+          }
+        }
+        break
+
+      case 'shake':
+        keyframes = this._createShake(_color, actionStrength, dimmer)
+        break
+
+      case 'strobe':
+        keyframes = this._createStrobe(_color, actionStrength, dimmer)
+        break
+
+      case 'oscillate':
+        keyframes = this._createOscillate(_color, actionStrength, dimmer)
+        break
+    
+      default:
+        break
+    }
+
+    console.log(JSON.stringify(keyframes))
+
+    store.dispatch(setAnimation({
+      // TODO: Get ID by name
+      id: "a5d47590-6908-11e9-8c5e-cf09166b76ae",
+      keyframes,
+      duration
+    }))
+
+    // Save the last value
+    this.lastValues = { dimmer }
+  }
+
+  _createShake(color, actionStrength, dimmer) {
+    let keyframes = {}
+    // Defines the number of steps, higher value means faster changes
+    const steps = 30 * actionStrength
+
+    for (let step = 0; step <= steps; step++) {
+      let _step = (1 / steps * step).toFixed(2)
+
+      keyframes[_step] = {}
+
+      // keyframes[_step].dimmer = step % 2 === 0 ? dimmer : 0
+      keyframes[_step].dimmer = Math.floor(Math.random() * dimmer) + 0
+
+      // First step
+      if (step === 0) {
+        keyframes[_step].color = color
+      }
+
+      // Last step
+      if (step === steps) {
+        keyframes[_step].color = color
+      }
+      
+    }
+
+    return keyframes
+  }
+
+  // TODO: Use strobe effect of the fixture instead
+  _createStrobe(color, actionStrength, dimmer) {
+    let keyframes = {}
+    // Defines the number of steps, higher value means faster changes
+    const steps = 50 * actionStrength
+
+    for (let step = 0; step <= steps; step++) {
+      let _step = (1 / steps * step).toFixed(2)
+
+      keyframes[_step] = {}
+      keyframes[_step + 0.15] = {}
+
+      keyframes[_step].dimmer = step % 2 === 0 ? dimmer : 0
+      keyframes[_step + 0.15].dimmer = step % 2 === 0 ? 0 : dimmer
+      // keyframes[_step].dimmer = Math.floor(Math.random() * dimmer) + 0
+
+      // First step
+      if (step === 0) {
+        keyframes[_step].color = color
+      }
+
+      // Last step
+      if (step === steps) {
+        keyframes[_step].color = color
+      }
+      
+    }
+
+    return keyframes
+  }
+
+  // TODO: Use strobe effect of the fixture instead
+  _createOscillate(color, actionStrength, dimmer) {
+    let keyframes = {}
+    // Defines the number of steps, higher value means faster changes
+    const steps = 30 * actionStrength
+
+    for (let step = 0; step <= steps; step++) {
+      let _step = (1 / steps * step).toFixed(2)
+
+      keyframes[_step] = {}
+
+      keyframes[_step].dimmer = step % 2 === 0 ? dimmer : 0
+
+      // First step
+      if (step === 0) {
+        keyframes[_step].color = color
+      }
+
+      // Last step
+      if (step === steps) {
+        keyframes[_step].color = color
+      }
+      
+    }
+
+    return keyframes
   }
 
   render() {
